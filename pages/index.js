0e825258d65e593d0bb7206bea961b013e6e1758
RaisulislamRimon <51745662+RaisulislamRimon@users.noreply.github.com>
2023-03-28 16:05:45 +0600

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <div>
@@ -5,6 +7,9 @@ export default function Home() {
       <h1>Hello world</h1>
       <h1>Hello world</h1>
       <h1>Hello world</h1>
+
+
+      <Link href="/blogs/100">/blogs/100</Link>
     </div>
   );
 }
