5e7dcc41962b254751a2f86289fd5b11c9080a03
RaisulislamRimon <51745662+RaisulislamRimon@users.noreply.github.com>
2023-03-28 17:01:39 +0600

    1 file modified

--- pages/posts/post.js
+++ pages/posts/post.js
@@ -1,12 +1,19 @@
+import Head from 'next/head';
 import Link from 'next/link';
 
 export default function Post({ post }) {
   return (
-    <div className="border rounded-lg p-10 m-5 hover:text-red-600 hover:cursor-pointer">
-      <h1 className="text-xl underline text-gray">Post</h1>
-      <Link href={`/posts/${post.id}`}>
-        <p>Post Id: {post.id}</p>
-      </Link>
+    <div>
+      <Head>
+        <title>Post</title>
+      </Head>
+
+      <div className="border rounded-lg p-10 m-5 hover:text-red-600 hover:cursor-pointer">
+        <h1 className="text-xl underline text-gray">Post</h1>
+        <Link href={`/posts/${post.id}`}>
+          <p>Post Id: {post.id}</p>
+        </Link>
+      </div>
     </div>
   );
 }
