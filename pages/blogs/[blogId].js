36b5949bd10b54736d0d8fabcb3b5c75137a2605
RaisulislamRimon <51745662+RaisulislamRimon@users.noreply.github.com>
2023-03-28 15:47:45 +0600

    1 file modified

--- pages/blogs/[blogId].js
+++ pages/blogs/[blogId].js
@@ -1,7 +1,12 @@
+import { useRouter } from 'next/router';
+
 export default function blogId() {
+  const router = useRouter();
+  const blogId = router.query.blogId;
   return (
     <div>
-      <h1>Dynamic Blog page created</h1>
+      <h1>Dynamic Blog page created </h1>
+      <p>Page No: : {blogId}</p>
     </div>
   );
 }
