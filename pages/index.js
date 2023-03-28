71b080e805ad7a4c460d20a2bf9e313e7293f941
RaisulislamRimon <51745662+RaisulislamRimon@users.noreply.github.com>
2023-03-28 16:42:44 +0600

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
-export default function Home() {
+export async function getStaticProps() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const posts = await res.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
+export default function Home({ posts }) {
   return (
     <div>
       <h1 className="text-xl font-bold underline">Hello world</h1>
@@ -12,6 +23,7 @@ export default function Home() {
       <Link href="/blogs/100">/blogs/100</Link> <br /> <br />
       <Image src="/flowers.jpg" width={120} height={120}></Image>
       <button className="btn btn-warning">Warning</button>
+      {posts.length}
     </div>
   );
 }
