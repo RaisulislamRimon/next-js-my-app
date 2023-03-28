50d219c37851d625a657decef872aefb25fd51f9
RaisulislamRimon <51745662+RaisulislamRimon@users.noreply.github.com>
2023-03-28 16:37:10 +0600

    4 files modified, 2 added

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,14 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div>
-      <h1>Hello world</h1>
+      <h1 className="text-xl font-bold underline">Hello world</h1>
       <h1>Hello world</h1>
       <h1>Hello world</h1>
       <h1>Hello world</h1>
       <Link href="/blogs">/blogs</Link> <br /> <br />
       <Link href="/blogs/100">/blogs/100</Link> <br /> <br />
       <Image src="/flowers.jpg" width={120} height={120}></Image>
+      <button className="btn btn-warning">Warning</button>
     </div>
   );
 }
