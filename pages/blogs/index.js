6f9a49cd6f40008d4b75b8a6906e40bab9be4ad3
RaisulislamRimon <51745662+RaisulislamRimon@users.noreply.github.com>
2023-03-28 17:02:27 +0600

    1 file modified

--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -1,8 +1,13 @@
 import Link from 'next/link';
+import Head from 'next/head';
 
 export default function blog() {
   return (
     <div>
+      <Head>
+        <title>Blogs</title>
+      </Head>
+
       <h1>This is blog main index page under blog folder</h1>
       <p>This is blog index page under blog folder</p>
       <Link href="/">Go to Home</Link>
