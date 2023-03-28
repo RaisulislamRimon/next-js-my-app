4ac60deed50398c1600bc37ffa083acbc288d79b
RaisulislamRimon <51745662+RaisulislamRimon@users.noreply.github.com>
2023-03-28 15:21:43 +0600

    1 file modified, 2 added

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,9 @@ export default function Home() {
   return (
     <div>
       <h1>Hello world</h1>
+      <h1>Hello world</h1>
+      <h1>Hello world</h1>
+      <h1>Hello world</h1>
     </div>
   );
 }
