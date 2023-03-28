f1b1029964c8cf62e0ea0a262d7c83876fb180da
RaisulislamRimon <51745662+RaisulislamRimon@users.noreply.github.com>
2023-03-28 15:33:04 +0600

    1 file modified

--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -1,7 +1,8 @@
 export default function blog() {
   return (
     <div>
-      <h1>This is blog index page under blog folder</h1>
+      <h1>This is blog main index page under blog folder</h1>
+      <p>This is blog index page under blog folder</p>
     </div>
   );
 }
