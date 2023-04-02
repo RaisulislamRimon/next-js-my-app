886e1662545783a2fdcc344d12cbf1f795a0c8ff
RaisulislamRimon <51745662+RaisulislamRimon@users.noreply.github.com>
2023-04-02 13:48:59 +0600

    1 file modified

--- components/Post/Post.js
+++ components/Post/Post.js
@@ -6,7 +6,8 @@ export default function Post({ post }) {
       <div className="border rounded-lg p-10 m-5 hover:text-red-600 hover:cursor-pointer">
         <h1 className="text-xl underline text-gray">Post</h1>
         <Link href={`/posts/${post.id}`}>
-          <p>Post Id: {post.id}</p>
+          <p className="underline text-orange-300">Post Id: {post.id}</p>
+          <p className="hover:text-orange-200">Post title: {post.title}</p>
         </Link>
       </div>
     </div>
