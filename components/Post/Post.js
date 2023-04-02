c3d5ec91cfcc9d3662e717a5122077329a147928
RaisulislamRimon <51745662+RaisulislamRimon@users.noreply.github.com>
2023-04-02 14:00:29 +0600

    1 file modified

--- components/Post/Post.js
+++ components/Post/Post.js
@@ -7,6 +7,9 @@ export default function Post({ post }) {
         <div className="border rounded-lg p-10 m-5 hover:text-red-600 hover:cursor-pointer">
           <p className="underline text-orange-300">Post Id: {post.id}</p>
           <p className="hover:text-orange-200">Post title: {post.title}</p>
+          <button className="btn btn-outline btn-accent mt-3">
+            See details
+          </button>
         </div>
       </Link>
     </div>
