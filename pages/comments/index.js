7b9f20f90d2262c6126964afafbe249a4134dee3
RaisulislamRimon <51745662+RaisulislamRimon@users.noreply.github.com>
2023-04-03 12:42:16 +0600

    1 file modified

--- pages/comments/index.js
+++ pages/comments/index.js
@@ -1,7 +1,31 @@
-export default function comments() {
+export default function comments({ data }) {
+  console.log(data);
   return (
     <div>
       <h1>Comments</h1>
+      {data.map((comment) => (
+        <div key={comment.id}>
+          <p>{comment.id}</p>
+          <h1>{comment.body}</h1>
+        </div>
+      ))}
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/comments`);
+  const data = await res.json();
+
+  // console.log(data);
+
+  if (!data) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: { data }, // will be passed to the page component as props
+  };
 }
